f2b0eed42d927d74d9f4a928ae2ddcaaf75e32af
Dut <dutandrew78@gmail.com>
2023-09-25 15:49:16 +0300

    Add 0-constants file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
-export function taskFirst() {
-  var task = 'I prefer const when I can.';
-  return task;
+export function taskFirst(){
+    const task = 'I prefer const when I can.';
+    return task;
 }
 
 export function getLast() {
-  return ' is okay';
+    return ' is okay';
 }
 
 export function taskNext() {
-  var combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
+    let combination = ' But sometimes let';
+    combination += getLast();
+    return combination;
 }
